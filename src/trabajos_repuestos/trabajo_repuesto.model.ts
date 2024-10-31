9ae201f9b564796b49bb132e91c4934b1567dd13
Pieshna <cabreraj525@gmail.com>
2024-10-30 22:18:44 -0600

    feat: Agregar métodos para obtener todos los trabajos y vales de repuestos por ID en los modelos TrabajoRepuesto y ValeTrabajo

--- src/trabajos_repuestos/trabajo_repuesto.model.ts
+++ src/trabajos_repuestos/trabajo_repuesto.model.ts
@@ -4,6 +4,21 @@ class TrabajoRepuestoModel extends DefaultModel {
   constructor() {
     super('trabajos_repuestos')
   }
+
+  async findAll() {
+    const sql = `select tr.*,r.nombre,r.lugar,r.precio,1 as omit from trabajos_repuestos as tr
+    join repuestos as r on tr.repuesto_id = r.id`
+    const res = await this.findByQuery(sql)
+    return res
+  }
+
+  async findById(uuid: string) {
+    const sql = `select tr.*,r.nombre,r.lugar,r.precio,1 as omit from trabajos_repuestos as tr
+    join repuestos as r on tr.repuesto_id = r.id
+    where tr.trabajo_id = $1`
+    const res = await this.findByQuery(sql, [uuid])
+    return res
+  }
 }
 
 export default new TrabajoRepuestoModel()
--- src/vales_trabajos/vale_trabajo.model.ts
+++ src/vales_trabajos/vale_trabajo.model.ts
@@ -4,6 +4,23 @@ class ValesTrabajosModel extends DefaultModel {
   constructor() {
     super('vales_trabajos')
   }
+
+  async findAll() {
+    const sql = `select vt.*,v.cantidad, r.nombre,r.precio,r.lugar,1 as omit from vales_trabajos vt
+    join vales as v on vt.vale_id = v.id
+    join repuestos as r on v.repuesto_id = r.id`
+    const res = await this.findByQuery(sql)
+    return res
+  }
+
+  async findById(uuid: string) {
+    const sql = `select vt.*,v.cantidad, r.nombre,r.precio,r.lugar, 1 as omit from vales_trabajos vt
+    join vales as v on vt.vale_id = v.id
+    join repuestos as r on v.repuesto_id = r.id
+    where vt.trabajo_id = $1`
+    const res = await this.findByQuery(sql, [uuid])
+    return res
+  }
 }
 
 export default new ValesTrabajosModel()
